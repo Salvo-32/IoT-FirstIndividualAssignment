d7099b7906670ca556a48f59c64c3f8f2d4f20b2
Salvo-32 <73128762+Salvo-32@users.noreply.github.com>
2021-04-12 19:04:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include "periph/gpio.h"  // https://doc.riot-os.org/gpio_8h.html
 #include "periph/adc.h"   // https://doc.riot-os.org/adc_8h.html
 #include "analog_util.h"  // https://doc.riot-os.org/analog__util_8h.html
-#include "thread.h"       //
-
+#include "thread.h"       // https://doc.riot-os.org/core_2include_2thread_8h.html
+#include "shell.h"        // https://doc.riot-os.org/shell_8h.html
 */
 
 
@@ -15,7 +15,8 @@
 #include "/home/salvo/RIOT/drivers/include/periph/gpio.h"  // https://doc.riot-os.org/gpio_8h.html
 #include "/home/salvo/RIOT/drivers/include/periph/adc.h"   // https://doc.riot-os.org/adc_8h.html
 #include "/home/salvo/RIOT/sys/include/analog_util.h"      // https://doc.riot-os.org/analog__util_8h.html
-#include "/home/salvo/RIOT/core/include/thread.h"
+#include "/home/salvo/RIOT/core/include/thread.h"          // https://doc.riot-os.org/core_2include_2thread_8h.html
+#include "/home/salvo/RIOT/sys/include/shell.h"            // https://doc.riot-os.org/shell_8h.html
 
 
 // Global Constants
@@ -31,20 +32,20 @@
     int analog_input_line[NUM_INPUT_LINE] = {0, 1}; //0 for Photoresistor, 1 for Thermistor
     int adc_res[NUM_INPUT_LINE] = {ADC_RES_10BIT, ADC_RES_12BIT}; //For the Photoresistor 10 bits are enough
     //int sample_delay[NUM_INPUT_LINE] = {10000LU * US_PER_MS, 7000LU * US_PER_MS}; // 500 ms per BRIGHTNESS, 100 ms per TEMPERATURE
-    int sample_delay[NUM_INPUT_LINE] = {10, 1}; // 10 s per BRIGHTNESS, 1 s per TEMPERATURE
+    int sample_delay[NUM_INPUT_LINE] = {10, 5}; // 10 s per BRIGHTNESS, 1 s per TEMPERATURE
 
     //Digital Outputs
     int port = PORT_A;
     int digital_pin[NUM_OUTPUT_LINE] = {10, 8, 9};
     gpio_t digital_input_line[NUM_OUTPUT_LINE]; //0 for FAN, 1 for LAMP, 2 for BUZZER
 
-    //Threads
-    char thread_stack[THREAD_STACKSIZE_MAIN]; //Stack for thread
-
+    //Threads - TODO Matrix of stack
+    char thread1_stack[THREAD_STACKSIZE_MAIN]; //Stack for thread
+    char thread2_stack[THREAD_STACKSIZE_MAIN];
 
 //Function, Procedures, Thread handlers
     //Thread for Brightness
-/*
+
 void *brightness_thread_handler(void *arg){
     (void) arg;
 
@@ -58,7 +59,7 @@ void *brightness_thread_handler(void *arg){
 		puts("I'm the BRIGHTNESS THREAD I'm ALWAYS active, but I sleep for 20 seconds\n");
 
         sample = adc_sample(analog_input_line[line_num], adc_res[line_num]);
-        if (sample == -1) {
+        if (sample == -1) { // ERROR in in sample resolution
             printf("ADC_LINE(%d): selected resolution not applicable\n", analog_input_line[line_num]);
             //return -1;
         }
@@ -81,7 +82,7 @@ void *brightness_thread_handler(void *arg){
     }
 	return NULL;
 }
-*/
+
 void *temperature_thread_handler(void *arg){
     (void) arg;
 
@@ -103,7 +104,7 @@ void *temperature_thread_handler(void *arg){
             physical_quantity = adc_util_map(sample, adc_res[line_num], 10, 100); //CELSIUS
             printf("ADC_LINE(%d): raw value: %i, CELSIUS: %i\n", analog_input_line[line_num], sample, physical_quantity);
             if(physical_quantity >= MAX_TEMPERATURE){
-                printf("ROOM TEMPERATURE is greather than threshold! WARNING!!! \n");
+                printf("ROOM TEMPERATURE is greater than threshold! WARNING!!! \n");
 
                 // Switch on the BUZZER for 10 seconds
                 gpio_write(digital_input_line[2], 1); //Switch ON 2 for BUZZER
@@ -135,8 +136,6 @@ int main(void){
     printf("\n RIOT HenHouse application \n");
 
     // Initialization of the Sensors
-    printf("\n Analog line:%d, with resolution: %d\n", analog_input_line[1], adc_res[1]);
-
         // initialize the ADC lines: 0 for Photoresistor, 1 for Thermistor
     for(i = 0; i < NUM_INPUT_LINE; i++){
         if ((res = adc_init(analog_input_line[i])) == -1) {
@@ -163,10 +162,10 @@ int main(void){
     gpio_write(digital_input_line[1], 1); // Relay Module works in NEGATED Logic (1 for LOW, 0 for HIGH)
 
     // Thread initialization
-/*
+
     kernel_pid_t brightness_pid = thread_create( //To improve with array of PID
-		thread_stack,
-		sizeof(thread_stack),
+		thread1_stack,
+		sizeof(thread1_stack),
     	THREAD_PRIORITY_MAIN - 1,
         THREAD_CREATE_STACKTEST,
         brightness_thread_handler,
@@ -174,10 +173,10 @@ int main(void){
         "BrightnessThread"
 	);
     printf("\n From MAIN, the brightness pid is: %d \n", brightness_pid);
-*/
+
     kernel_pid_t temperature_pid = thread_create( //To improve with array of PID
-        thread_stack,
-		sizeof(thread_stack),
+        thread2_stack,
+		sizeof(thread2_stack),
     	THREAD_PRIORITY_MAIN - 2,
         THREAD_CREATE_STACKTEST,
         temperature_thread_handler,
@@ -185,21 +184,6 @@ int main(void){
         "TemperatureThread"
 	);
     printf("\n From MAIN, the temperature pid is: %d", temperature_pid);
-
-/*
-	if(pid == EINVAL){
-		printf("\n Error: Priority of the thread greater than or equal  error %d\n", SCHED_PRIO_LEVELS);
-		return 1;
-	}
-	else if(pid == EOVERFLOW){
-		printf("\n Error: Too many thread already running \n");
-		return 1;
-	}
-	else{
-		printf("Thread %d created correctly \n", pid);
-	}
-*/
-
 
     return 0;
 }
